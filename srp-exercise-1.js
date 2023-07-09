c8a9249d1b9ec6656592557eb344a74cd7df69ce
StocktonManges <stocktonmanges@gmail.com>
2023-07-09 17:31:51 -0600

    Exercise complete.

--- srp-exercise-1.js
+++ srp-exercise-1.js
@@ -9,24 +9,30 @@
 * validate the email and assign it to the Person instance if valid.
 */
 
-class Person {
-  constructor(name, email){
-      this.name = name;
-      if(this.validateEmail(email)) {
-        this.email = email;
-      }
-      else {
-          throw new Error("Invalid email!");
-      }
+class Email {
+  constructor(email){
+    if(this.validateEmail(email)) {
+      this.email = email;
+    }
+    else {
+        throw new Error("Invalid email!");
+    }
   }
   validateEmail(email) {
-      const re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
-      return re.test(email);
+    const re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+    return re.test(email);
+  }
+}
+
+class Person {
+  constructor(name, email) {
+    this.name = name;
+    this.email = email;
   }
   greet() {
     console.log(`Hello, my name is ${this.name}`);
   }
 }
 
-const me = new Person("James", "code@life.com");
+const me = new Person("James", new Email("code@life.com"));
 console.log(me);
